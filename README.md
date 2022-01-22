eaaa1c3bdf1109ed655e6a4425ca551a222ac344
문지기 <su10301@sonline20.sen.go.kr>
2022-01-22 16:15:06 +0900

    week1

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# fghk
+"""
+way 1
+a="banana"
+b="kangyuill"
+print("%s는%s가 좋아하는 과일이다")
+print(a,b)
+
+way 2 
+print(f"{a}는{b}가 좋아하는 과일이다")
+
+sep
+print("강유일","바나나")
+print(a,b,a,b)
+print(a,b,a,b,sep=",")
+"""
+str=input(이름을 입력하세요:")
--- kjg.py
+++ kjg.py
@@ -1,7 +1,24 @@
-print("Hello world")
-a=3
-b=2
-c=a+b
-print(c)
-print(decide
-      )
+
+"""
+way 1
+a="banana"
+b="kangyuill"
+print("%s는%s가 좋아하는 과일이다")
+print(a,b)
+
+way 2
+print(f"{a}는{b}가 좋아하는 과일이다")
+
+sep
+print("강유일","바나나")
+print(a,b,a,b)
+print(a,b,a,b,sep=",")
+"""
+str=input("이름을 입력하세요>>>")
+print("%s님 안녕하세요."%str)
+
+#mission
+name=input("이름을 입력하세요>>>")
+say=input("반갑습니다")
+print(f"{name}은 {say}라고 말했다")
+
